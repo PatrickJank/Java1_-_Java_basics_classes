6b52ddeb28b583e9de63a8e8b65e525b1c71c434
PatrickJank <format97@wp.pl>
2022-11-11 19:29:40 +0100

    Updates to Human class

--- src/Human.java
+++ src/Human.java
@@ -1,7 +1,26 @@
-public class Human {
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
+//importing data libraries for the task 3/4.2
+ class Human {
     Animal pet;
     String firstName;
     String lastName;
 
-Car car;
+    private Car car;
+
+    private Double salary = 4500.0;
+
+    public Double getSalary() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println("Information was generated on " + dtf.format(now));
+        return (this.salary);
+    }
+    public Double setSalary(Double value) {
+        this.salary = value;
+        return value;
+    }
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,54 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-    Animal cat = new Animal("Cat");
-    cat.name = "Tomek";
+        Animal cat = new Animal("Cat");
+        cat.name = "Tomek";
 
-    //before feeding
-    System.out.println(cat.weight);
-    cat.Feed();
-    //after feeding
-    System.out.println(cat.weight);
+        //before feeding
+        System.out.println(cat.weight);
+        cat.Feed();
+        //after feeding
+        System.out.println(cat.weight);
 
-    //taking for a walk until weight = 0
+        //taking for a walk until weight = 0, uncomment to trigger
+        /*
     for(int i = 30; i >0 ; i--) {
         cat.takeForAWalk();
     }
     System.out.println(cat.weight);
+*/
 
-    //creating a new Human object, assigning previously created cat to the human
-Human me = new Human();
-me.firstName="Patrick";
-me.lastName="Jankiewicz";
-me.pet = cat;
+        //creating a new Human object, assigning previously created cat to the human
+        Human me = new Human();
+        me.firstName = "Patrick";
+        me.lastName = "Jankiewicz";
+        me.pet = cat;
 
-Car car = new Car("Skoda","Octavia");
-me.car = car;
-me.car.YearOfProduction = 2009;
-me.car.weight = 5000;
+        Car car = new Car("Skoda", "Octavia");
+        me.car = car;
+        me.car.YearOfProduction = 2009;
+        me.car.weight = 5000;
+
+//Display current salary
+        System.out.println("Your current salary is " + me.getSalary());
+        Scanner decision = new Scanner(System.in);
+        System.out.println("Are you sure you want to change your salary? (Y/N)");
+        char x = decision.next().charAt(0);
+        char Y = 'Y';
+        if (x == Y)
+        {
+            System.out.println("Please enter your new salary. Use (0000,0) format");
+                if (x < 0)
+                    System.out.println("Please do not use negative values");
+                else {
+                    double newSalary = decision.nextDouble();
+                    me.setSalary(newSalary);
+                    System.out.println("Your new salary has been sent to the billing department, please remember to get a new anex from Mr. Hania");
+                    System.out.println("By the way, ZUS and US already know about your new salary, no point in hiding it");
+                }
+            }
 
 
 
-
-
-
-
-
+        }
     }
-}
