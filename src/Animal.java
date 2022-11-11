b28058e595217551ab902ed4a729a7889b10842a
PatrickJank <format97@wp.pl>
2022-11-11 10:27:15 +0100

    Finalizing the "animal" class, and creating 2 methods for it.

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,44 @@
 public class Animal {
+
+    final String species;
+    Double weight;
+
+    String name;
+
+    Boolean isAlive;
+Animal(String x)
+{
+    this.isAlive = true;
+    species = x;
+    if(x == "dog" || x =="Dog")
+        weight = 10.0;
+    if(x == "cat" || x == "Cat")
+        weight = 5.0;
+    else
+        weight = 2.0;
+}
+
+void Feed()
+    {
+        if(this.isAlive){
+            this.weight += 0.1;
+            System.out.println("Om nom nom");
+    }
+   if(!this.isAlive)
+        System.out.println("He's dead, why are you doing this?");
+    }
+void takeForAWalk()
+    {
+        if(this.isAlive)
+            this.weight -= 0.2;
+        if(this.weight<= 0.0)
+        {
+            this.isAlive = false;
+            this.weight = 0.0;
+        }
+        if(!this.isAlive)
+        System.out.println("The police is on their way you sicko");
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       // System.out.println("Hello world!");
+    Animal cat = new Animal("Cat");
+    cat.name = "Tomek";
+
+    //before feeding
+    System.out.println(cat.weight);
+    cat.Feed();
+    //after feeding
+    System.out.println(cat.weight);
+
+    //taking for a walk until weight = 0
+    for(int i = 30; i >0 ; i--) {
+        cat.takeForAWalk();
+    }
+    System.out.println(cat.weight);
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
